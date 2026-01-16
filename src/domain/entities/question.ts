1591457f9da535db3bd9d0172727a7cd00f6c72f
navestelar <acch.escola@gmail.com>
2026-01-15 22:06:37 -0300

    refactor: ddd - Optional type

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,13 +1,29 @@
 import { randomUUID } from "node:crypto"
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
 
 interface QuestionProps {
+  authorId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
   title: string
   content: string
   slug: Slug
-  authorId: string
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {
+  static create(
+    props: Optional<QuestionProps, 'createdAt'>,
+    id?: UniqueEntityID
+  ) {
+    const question = new Question({
+      ...props,
+      createdAt: new Date(),
+    }, id)
+
+    return question
+  }
 }
