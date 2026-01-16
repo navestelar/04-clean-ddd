65ed7177e774e5daddaa3254f737a78c3b958aec
navestelar <acch.escola@gmail.com>
2026-01-15 22:18:43 -0300

    refactor: alias

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,8 +1,8 @@
 import { randomUUID } from "node:crypto"
 import { Slug } from "./value-objects/slug"
-import { Entity } from "../../core/entities/entity"
-import { UniqueEntityID } from "../../core/entities/unique-entity-id"
-import { Optional } from "../../core/types/optional"
+import { Entity } from "@/core/entities/entity"
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+import { Optional } from "@/core/types/optional"
 import dayjs from "dayjs"
 
 interface QuestionProps {
