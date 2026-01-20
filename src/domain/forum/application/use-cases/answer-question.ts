bd02fcf3f5eaf7013635ad4ffcee1004901a7b60
navestelar <acch.escola@gmail.com>
2026-01-19 21:30:29 -0300

    refactor: clean archtecture

--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -1,6 +1,6 @@
-import { UniqueEntityID } from '../../core/entities/unique-entity-id'
-import { Answer } from '../entities/answer'
-import { AnswersRepository } from '../repositories/answers-repository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
