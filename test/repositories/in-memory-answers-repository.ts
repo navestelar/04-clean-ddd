1c18d1d35c0f0f230901781efad9d4c3c504f861
navestelar <acch.escola@gmail.com>
2026-01-20 22:23:44 -0300

    feat: question answers

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -14,8 +14,22 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     return answer
   }
 
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
+  }
+
   async create(answer: Answer) {
     this.items.push(answer)
+  }
+
+  async save(answer: Answer) {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items[itemIndex] = answer
   }
 
   async delete(answer: Answer) {
